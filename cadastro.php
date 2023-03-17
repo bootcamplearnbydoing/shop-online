88469348e49c31f90cb03ac6816f6f78ece69617
Vanessa Bonfim <vanesssabonfim@gmail.com>
2023-03-17 10:02:10 +0000

    Create the form base struture;

---
+++ cadastro.php
@@ -0,0 +1,77 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro | Shop</title>
+</head>
+
+<body>
+    <header class="cadastro__cabecalho">
+        <img class="cadastro__imagem" alt="Logo do Shop">
+        <h1 class="cadastro__titulo">ShopOnline</h1>
+    </header>
+    <main>
+        <section class="cartao">
+            <h2 class="cartao__titulo">Complete o seu cadastro!</h2>
+            <form action="" class="formulario">
+            <fieldset>
+                <legend class="formulario__titulo">Informações Básicas</legend>
+                    <div>
+                        <input class="input" type="text">
+                        <label class="input__label" for="">Nome</label>
+                        <span class="input__mensagem-erro">Este campo não está válido!</span>
+                    </div>
+                    <div>
+                        <input class="input" type="text">
+                        <label class="input__label" for="">Email</label>
+                        <span class="input__mensagem-erro">Este campo não está válido!</span>
+                    </div>
+                    <div>
+                        <input class="input" type="text">
+                        <label class="input__label" for="">Senha</label>
+                        <span class="input__mensagem-erro">Este campo não está válido!</span>
+                    </div>
+            </fieldset>
+            <fieldset>
+                <legend class="formulario__titulo">Informações Pessoais</legend>
+                    <div>
+                        <input class="input" type="text">
+                        <label class="input__label" for="">Data de Nascimento</label>
+                        <span class="input__mensagem-erro">Este campo não está válido!</span>
+                    </div>
+                    <div>
+                        <input class="input" type="text">
+                        <label class="input__label" for="">CPF</label>
+                        <span class="input__mensagem-erro">Este campo não está válido!</span>
+                    </div>
+            </fieldset>
+            <fieldset>
+                <legend class="formulario__titulo">Endereço</legend>
+                    <div>
+                        <input class="input" type="text">
+                        <label class="input__label" for="">CEP</label>
+                        <span class="input__mensagem-erro">Este campo não está válido!</span>
+                    </div>
+                    <div>
+                        <input class="input" type="text">
+                        <label class="input__label" for="">Logradouro</label>
+                        <span class="input__mensagem-erro">Este campo não está válido!</span>
+                    </div>
+                    <div>
+                        <input class="input" type="text">
+                        <label class="input__label" for="">Cidade</label>
+                        <span class="input__mensagem-erro">Este campo não está válido!</span>
+                    </div>
+                    <div>
+                        <input class="input" type="text">
+                        <label class="input__label" for="">Estado</label>
+                        <span class="input__mensagem-erro">Este campo não está válido!</span>
+                    </div>
+            </fieldset>
+            </form>
+        </section>
+    </main>
+</body>
+
+</html>
