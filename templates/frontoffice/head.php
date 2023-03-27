cdd31699a837aa610cd7a2883cd799290a082e24
Learn By Doing <bootcamplearnbydoing@gmail.com>
2023-03-27 23:13:55 +0100

    Co-authored-by: Vanessa Bonfim <vanessa-bonfim@users.noreply.github.com> Co-authored-by: Bianca Carvalho <Bianca-L-Carvalho@users.noreply.github.com>

---
+++ templates/frontoffice/head.php
@@ -0,0 +1,22 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro | Shop</title>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/asset/css/base/base.css">
+    <link rel="stylesheet" href="/asset/css/cadastro.css">
+    <link rel="stylesheet" href="/asset/css/componentes/cartoes.css">
+</head>
+
+<body>
+
+    <header class="container flex flex--column cadastro-cabecalho">
+        <img class="cadastro-cabecalho__logo" src="./asset/img/logoshop.jpg" alt="Logo do Shop">
+        <h1 class="cadastro-cabecalho__titulo">ShopOnline</h1>
+    </header>
