88dda383e9a60be3c2cbe63db0b06c642ac721c0
Learn By Doing <bootcamplearnbydoing@gmail.com>
2023-03-31 21:46:04 +0100

    feat: template engine  created.
    Co-authored-by: Vanessa Bonfim <vanessa-bonfim@users.noreply.github.com>

--- templates/frontoffice/head.php
+++ templates/frontoffice/head.php
@@ -9,14 +9,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
         integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/asset/css/base/base.css">
-    <link rel="stylesheet" href="/asset/css/cadastro.css">
-    <link rel="stylesheet" href="/asset/css/componentes/cartoes.css">
+    <link rel="stylesheet" href="public/css/base/base.css">
+    <link rel="stylesheet" href="public/css/cadastro.css">
+    <link rel="stylesheet" href="public/css/componentes/cartoes.css">
 </head>
 
 <body>
 
     <header class="container flex flex--column cadastro-cabecalho">
-        <img class="cadastro-cabecalho__logo" src="./asset/img/logoshop.jpg" alt="Logo do Shop">
+        <img class="cadastro-cabecalho__logo" src="public/img/logoshop.jpg" alt="Logo do Shop">
         <h1 class="cadastro-cabecalho__titulo">ShopOnline</h1>
     </header>
