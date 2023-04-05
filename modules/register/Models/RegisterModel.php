bc6023892f30cb78f6cd052435271f814511c037
Learn By Doing <bootcamplearnbydoing@gmail.com>
2023-04-05 22:11:39 +0100

    feat: repository created imp: some fields created feat: service created

--- modules/register/Models/RegisterModel.php
+++ modules/register/Models/RegisterModel.php
@@ -7,9 +7,17 @@ namespace Pet\Store\Register\Models;
 class RegisterModel
 {
     public function __construct(
-        readonly string $name,
-        readonly string $email,
-        readonly string $password
+        public readonly string $firstName,
+        public readonly string $lastName,
+        public readonly string $email,
+        public readonly string $password,
+        public readonly ?string $passwordConfirm,
+        public readonly ?string $birthDate,
+        public readonly ?string $address,
+        public readonly ?string $city,
+        public readonly ?string $postalCode,
+        public readonly ?string $billingAddress,
+        public readonly ?string $newsletter
     ) {}
 }
 
---
+++ modules/register/Repositories/RegisterRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pet\Store\Register\Services;
+
+use Pet\Store\Register\Repositories\Repository;
+
+class RegisterRepository implements Repository
+{
+    public function findAll(): void
+    {
+        
+    }
+
+    public function findById(int $id): void
+    {
+
+    }
+}
